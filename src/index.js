ffe5eea45798c3969dc70a9a4315cbe30504bf01
GersonVis <visoso126@gmail.com>
2022-04-20 16:52:49 -0500

    componente clase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './index.css';
 import { render } from '@testing-library/react';
 
-
-const App=()=>{
- return <div>HOla mundo</div>
-
+// this is other form of initialized a component
+class App extends Component{
+  render(){
+    return <h1>Hola usuario</h1>
+  }
 }
-
 render(<App></App>, document.getElementById("root"))
