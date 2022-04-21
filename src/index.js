8b1e3b7756db64186bb02a884a4cce78b3d69c33
GersonVis <visoso126@gmail.com>
2022-04-20 21:06:15 -0500

    use effect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './index.css';
 import { render } from '@testing-library/react';
 
+const Counter=()=>{
+  const [counter, setCounter]=useState(0)
+  useEffect(()=>{
+    console.log("i have updated")
+    return ()=>{
+      alert("i have deleted")
+    }
+  },[])
+  return <div>
+    <p>Counter: {counter}</p>
+    <button onClick={()=>setCounter(counter+1)}>+</button>
+    <button onClick={()=>setCounter(counter-1)}>-</button>
+  </div>
+}
 
 const App=()=>{
- return <div>HOla mundo</div>
+  const [showHide, setShowHide]=useState(true)
+ return <div>
+    <button onClick={()=>setShowHide(showHide==false)}>Eliminar</button>
+    {showHide && <Counter></Counter>}
+ </div>
 
 }
 
