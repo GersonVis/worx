9a320fa5b57b6afd57252213a5e2f2f1c7ba6ffa
GersonVis <visoso126@gmail.com>
2022-04-20 21:29:50 -0500

    events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { render } from '@testing-library/react';
 
+const GetText=()=>{
+  const[name, setName]=useState('')
+  return <dvi>
+    <input type="text" placeholder='name' onChange={(evt)=>setName(evt.target.value)}></input>
+    <p>Name: {name}</p>
+  </dvi>
+}
 
 const App=()=>{
- return <div>HOla mundo</div>
+ return <div><GetText></GetText></div>
 
 }
 
