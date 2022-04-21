05df74b54ecabcb1db65cea10ef1dbb504954522
GersonVis <visoso126@gmail.com>
2022-04-20 19:35:08 -0500

    state exercise

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import { render } from '@testing-library/react';
 
+const Counter=()=>{
+  const[counter, setCounter]=useState(0)
+  function evtClick(){//event increment counter
+    setCounter(counter+1)
+  }
+  return <div>
+    <p>Presionado: {counter}</p>
+    <button onClick={evtClick}>Aumentar</button>
+  </div>
+}
 
 const App=()=>{
- return <div>HOla mundo</div>
-
+ return <div><Counter></Counter></div>
 }
 
 render(<App></App>, document.getElementById("root"))
