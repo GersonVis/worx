ecb18009162174bc25c107eb2c6854f9ac3fa682
GersonVis <visoso126@gmail.com>
2022-04-21 16:05:17 -0500

    splitting

--- src/components/Sorprise.js
+++ src/components/Sorprise.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sorprise=()=>{
-    return <h1>Im sorprise</h1>
+    return <h1>I'm a sorprise</h1>
 }
 
 export default Sorprise; 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { render } from '@testing-library/react';
 import Sorprise from './components/Sorprise';
 
 const App = () => {
-    const[showSorprise, setShowSorprise]=use
+    const[showSorprise, setShowSorprise]=useState(false)
     return <div>
-        <Sorprise></Sorprise>
+        <button onClick={()=>setShowSorprise(showSorprise==false)}>Show sorprise</button>
+        {showSorprise && <Sorprise></Sorprise>}
         </div>
 
 }
